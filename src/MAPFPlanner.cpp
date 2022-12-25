6fbc8a16fe0499c8c1bc9223924028b3328c2fc3
Han Zhang <hanzhang@Hans-MacBook-Pro.local>
2022-12-24 16:03:36 -0800

    remove files not needed and simplify the code

---
+++ src/MAPFPlanner.cpp
@@ -0,0 +1,11 @@
+#include <MAPFPlanner.h>
+
+
+void MAPFPlanner::initialize(int preprocess_time_limit){
+  
+}
+
+// return next states for all agents
+std::vector<State> MAPFPlanner::plan(int time_limit){
+  return env->curr_states;
+}
