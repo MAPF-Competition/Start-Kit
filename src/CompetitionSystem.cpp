745fbc0bf1f1b6b65ecbb35e80ebb213e20bfdf5
Han Zhang <hanzhang@Hans-MacBook-Pro.local>
2023-02-06 19:31:39 -0800

    load benchmark map

--- src/CompetitionSystem.cpp
+++ src/CompetitionSystem.cpp
@@ -18,12 +18,31 @@ bool CompetitionSystem::load_map(string fname){
     size_t pos = fname.rfind('.');  // position of the file extension
     map_name = fname.substr(0, pos);  // get the name without extension
     getline (myfile, line);
-    boost::char_separator<char> sep(",");
-    boost::tokenizer< boost::char_separator<char> > tok(line, sep);
-    boost::tokenizer< boost::char_separator<char> >::iterator beg = tok.begin();
-    rows = atoi((*beg).c_str());  // read number of rows
-    beg++;
-    cols = atoi((*beg).c_str());  // read number of cols
+
+    if (line[0] == 't') {
+      // Benchmark 
+        boost::char_separator<char> sep(" ");
+      getline(myfile, line);
+      boost::tokenizer<boost::char_separator<char>> tok(line, sep);
+      boost::tokenizer<boost::char_separator<char>>::iterator beg;
+      beg = tok.begin();
+      beg++;
+      rows = atoi((*beg).c_str()); // read number of rows
+      getline(myfile, line);
+      boost::tokenizer<boost::char_separator<char>> tok2(line, sep);
+      beg = tok2.begin();
+      beg++;
+      cols = atoi((*beg).c_str()); // read number of cols
+      getline(myfile, line); // skip "map"
+
+    } else {
+      boost::char_separator<char> sep(",");
+      boost::tokenizer< boost::char_separator<char> > tok(line, sep);
+      boost::tokenizer< boost::char_separator<char> >::iterator beg = tok.begin();
+      rows = atoi((*beg).c_str());  // read number of rows
+      beg++;
+      cols = atoi((*beg).c_str());  // read number of cols
+    }
 
     moves[0] = 1;
     moves[1] = -cols;
@@ -39,10 +58,10 @@ bool CompetitionSystem::load_map(string fname){
         getline(myfile, line);
         for (int j = 0; j < cols; j++) {
             int id = cols * i + j;
-            if (line[j] == '@') // obstacle
-                map[id] = 1;
-            else  // free space
+            if (line[j] == '.') // free space
                 map[id] = 0;
+            else   // obstacle
+                map[id] = 1;
         }
     }
 
