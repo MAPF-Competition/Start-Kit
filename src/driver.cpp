ce6ad331c5bdb2fe2b4481592eeaf97611878e9d
Han Zhang <hanzhang@Hans-MacBook-Pro.local>
2023-02-06 19:32:17 -0800

    clean up unnecessary options in main

--- src/driver.cpp
+++ src/driver.cpp
@@ -11,18 +11,14 @@ int main(int argc, char** argv) {
 		("help", "produce help message")
 		("map,m", po::value<std::string>()->required(), "input map file")
 		("task", po::value<std::string>()->required(), "input task file")
-		("plannerPath,po", po::value<std::string>()->default_value("../exp/test_planner"), "planner path file name")
-		("actualPath,ao", po::value<std::string>()->default_value("../exp/test_actual"), "actual path file name")
-		("output,o", po::value<std::string>()->default_value("../exp/test"), "output folder name")
+		("plannerPath,po", po::value<std::string>()->default_value("./exp/test_planner"), "planner path file name")
+		("actualPath,ao", po::value<std::string>()->default_value("./exp/test_actual"), "actual path file name")
+		("output,o", po::value<std::string>()->default_value("./exp/test"), "output folder name")
 		("cutoffTime,t", po::value<int>()->default_value(60), "cutoff time (seconds)")
 		("seed,d", po::value<int>(), "random seed")
 		("screen,s", po::value<int>()->default_value(1), "screen option (0: none; 1: results; 2:all)")
 		("simulation_time", po::value<int>()->default_value(5000), "run simulation")
-		("travel_time_window", po::value<int>()->default_value(0), "consider the traffic jams within the given window")
-		("planning_window", po::value<int>()->default_value(INT_MAX / 2),
-		        "the planner outputs plans with first planning_window timesteps collision-free")
 		("checkconf", po::value<bool>()->default_value(true), "consider conflict")
-		("log", po::value<bool>()->default_value(false), "save the search trees (and the priority trees)")
 		;
 	clock_t start_time = clock();
 	po::variables_map vm;
@@ -38,12 +34,6 @@ int main(int argc, char** argv) {
     // make dictionary
 	boost::filesystem::path dir(vm["output"].as<std::string>() +"/");
 	boost::filesystem::create_directories(dir);
-	if (vm["log"].as<bool>()) {
-		boost::filesystem::path dir1(vm["output"].as<std::string>() + "/goal_nodes/");
-		boost::filesystem::path dir2(vm["output"].as<std::string>() + "/search_trees/");
-		boost::filesystem::create_directories(dir1);
-		boost::filesystem::create_directories(dir2);
-	}
 
   	MAPFPlanner* planner = new MAPFPlanner();
 
