408d908c8046f9b1424a6546cd4c942db03a9405
Teng Guo <2274880117@qq.com>
2023-03-09 11:29:49 -0500

    update Cmake

--- python/pyMAPFPlanner.cpp
+++ python/pyMAPFPlanner.cpp
@@ -2,16 +2,18 @@
 
 
 pyMAPFPlanner::pyMAPFPlanner():MAPFPlanner(){
+    auto sys=pybind11::module_::import("sys");
+    sys.attr("path").attr("append")("./python");
     auto py_mapf_planner_module=pybind11::module_::import("pyMAPFPlanner");
     std::cout<<"trying to import pyMAPFPlanner module"<<std::endl;
-    py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")();
+    py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")(env);
     std::cout<<"pyMAPF Planner Created!"<<std::endl;
 }
 
 
 void pyMAPFPlanner::initialize(int preprocess_time_limit){
-    py_planner.attr("initialize")();
-    env=py_planner.attr("env").cast<SharedEnvironment*>();
+    py_planner.attr("initialize")(preprocess_time_limit);
+    // env=py_planner.attr("env").cast<SharedEnvironment*>();
 }
 
 
