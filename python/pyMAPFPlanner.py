378e09d7edca3b02141176a6b5a6f5d2360670a1
Teng Guo <2274880117@qq.com>
2023-03-10 12:38:49 -0500

    add _single_agent

--- python/pyMAPFPlanner.py
+++ python/pyMAPFPlanner.py
@@ -16,6 +16,7 @@ class pyMAPFPlanner:
             preprocess_time_limit (_type_): _description_
         """
         pass
+        print("planner initialize done" )
         # raise NotImplementedError()
 
     def plan(self,time_limit):
@@ -31,16 +32,17 @@ class pyMAPFPlanner:
         # example of only using single-agent search
         actions=[ MAPF.Action.W for i in range(len(self.env.curr_states))]
         for i in range(0,self.env.num_of_agents):
-            print("start plan for agent ",i)
+            print("start plan for agent ",i,end=" ")
             path=[]
-            if len(self.env.goal_locations[i]):
-                print(i," does not have any goal left")
+            if len(self.env.goal_locations[i])==0:
+                print(i," does not have any goal left",end=" ")
                 path.append((self.env.curr_states[i].location,self.env.curr_states[i].orientation))
             else:
-                print( " with start and goal: ")
+                print( " with start and goal: ",end=" ")
                 path=self.single_agent_plan(self.env.curr_states[i].location,self.env.curr_states[i].orientation,self.env.goal_locations[i][0][0])
+
             print("current location:",path[0][0],"current direction: ",path[0][1])
-            if path[0][0]!=self.env.curr_states[i].orientation:
+            if path[0][0]!=self.env.curr_states[i].location:
                 actions[i]=MAPF.Action.FW
             elif path[0][1]!=self.env.curr_states[i].orientation:
                 incr=path[0][1]-self.env.curr_states[i].orientation
@@ -48,6 +50,7 @@ class pyMAPFPlanner:
                     actions[i]=MAPF.Action.CR
                 elif incr==-1 or incr==3:
                     actions[i]=MAPF.Action.CCR
+        # print(actions)
         return actions
         # print("python binding debug")
         # print("env.rows=",self.env.rows,"env.cols=",self.env.cols,"env.map=",self.env.map)
@@ -55,6 +58,7 @@ class pyMAPFPlanner:
 
 
     def single_agent_plan(self, start:int,start_direct:int,end:int):
+        print(start,start_direct)
         path=[]
         # AStarNode (u,dir,t,f)
         open_list=PriorityQueue()
@@ -62,24 +66,29 @@ class pyMAPFPlanner:
         open_list.put(s,0)
         all_nodes=dict()
         close_list=set()
-        parent={start:None}
+        parent={(start,start_direct):None}
         all_nodes[start*4+start_direct]=s
         while not open_list.empty():
             curr=open_list.get()
             close_list.add(curr[0]*4+curr[1])
             if curr[0]==end:
+                curr=(curr[0],curr[1])
                 while curr!=None:
-                    path.append((curr[0],curr[1]))
+                    path.append(curr)
                     curr=parent[curr]
+                path.pop()
                 path.reverse()
+                
                 break
             neighbors=self.getNeighbors(curr[0],curr[1])
+            # print("neighbors=",neighbors)
             for neighbor in neighbors:
-                if (neighbor[0]*4+neighbor[1]) not in close_list:
+                if (neighbor[0]*4+neighbor[1])  in close_list:
                     continue
                 next_node=(neighbor[0],neighbor[1],curr[3]+1,self.getManhattanDistance(neighbor[0],end))
-                parent[next_node]=curr
+                parent[(next_node[0],next_node[1])]=(curr[0],curr[1])
                 open_list.put(next_node,next_node[3])
+        # print(path)
         return path
 
 
@@ -118,6 +127,7 @@ class pyMAPFPlanner:
         if (new_direction == 4):
             new_direction = 0
         neighbors.append((location,new_direction))
+        # print("debug!!!!!!!", neighbors)
         return neighbors
 
 
