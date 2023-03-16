3f9c87af6802c342321ed4a8a27e1d0b88cce747
Teng Guo <2274880117@qq.com>
2023-03-16 15:59:31 -0400

    numpy

--- python/pyMAPFPlanner.cpp
+++ python/pyMAPFPlanner.cpp
@@ -8,7 +8,7 @@ pyMAPFPlanner::pyMAPFPlanner():MAPFPlanner(){
     auto py_mapf_planner_module=pybind11::module_::import("pyMAPFPlanner");
     std::cout<<"trying to import pyMAPFPlanner module"<<std::endl;
     // py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")(py_env);
-    py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")(env);
+    py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")(py_env);
     std::cout<<"pyMAPF Planner Created!"<<std::endl;
 }
 
@@ -20,7 +20,18 @@ void pyMAPFPlanner::initialize(int preprocess_time_limit){
 
 
 std::vector<Action> pyMAPFPlanner::plan(int time_limit){
-    return py_planner.attr("plan")(time_limit).cast<std::vector<Action>>();
+    auto action_object=py_planner.attr("plan")(time_limit);
+    try{
+        return action_object.cast<std::vector<Action>>();
+    }   
+    catch(pybind11::cast_error e){
+        std::vector<int> tmp_action=action_object.cast<std::vector<int>>();
+        std::vector<Action> actions;
+        for(auto &ai:tmp_action){
+            actions.push_back(static_cast<Action>(ai));
+        }
+        return actions;
+    }
 }
 
 
