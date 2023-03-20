81485c563abe01f7de0a8893ee592b05a35b9cf7
Teng Guo <2274880117@qq.com>
2023-03-20 16:42:55 -0400

    fix bugs of python planner

--- python/pyMAPFPlanner.cpp
+++ python/pyMAPFPlanner.cpp
@@ -1,5 +1,5 @@
 #include"pyMAPFPlanner.hpp"
-
+#include <unistd.h>
 
 pyMAPFPlanner::pyMAPFPlanner():MAPFPlanner(){
     auto sys=pybind11::module_::import("sys");
@@ -7,14 +7,18 @@ pyMAPFPlanner::pyMAPFPlanner():MAPFPlanner(){
     sys.attr("path").attr("append")("./python");
     auto py_mapf_planner_module=pybind11::module_::import("pyMAPFPlanner");
     std::cout<<"trying to import pyMAPFPlanner module"<<std::endl;
-    // py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")(py_env);
     py_planner=py_mapf_planner_module.attr("pyMAPFPlanner")(py_env);
+
     std::cout<<"pyMAPF Planner Created!"<<std::endl;
 }
 
 
 void pyMAPFPlanner::initialize(int preprocess_time_limit){
+    std::cout<<"pyMAPFPlanner begin to initialize"<<std::endl;
     py_planner.attr("initialize")(preprocess_time_limit);
+
+
+
     // env=py_planner.attr("env").cast<SharedEnvironment*>();
 }
 
@@ -22,7 +26,9 @@ void pyMAPFPlanner::initialize(int preprocess_time_limit){
 std::vector<Action> pyMAPFPlanner::plan(int time_limit){
     auto action_object=py_planner.attr("plan")(time_limit);
     try{
-        return action_object.cast<std::vector<Action>>();
+        auto actions=action_object.cast<std::vector<Action>>();
+        assert(actions.empty()==false);
+        return actions;
     }   
     catch(pybind11::cast_error e){
         std::vector<int> tmp_action=action_object.cast<std::vector<int>>();
@@ -30,6 +36,7 @@ std::vector<Action> pyMAPFPlanner::plan(int time_limit){
         for(auto &ai:tmp_action){
             actions.push_back(static_cast<Action>(ai));
         }
+        assert(actions.empty()==false);
         return actions;
     }
 }
