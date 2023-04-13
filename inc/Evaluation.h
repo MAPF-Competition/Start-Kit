f0a9eb0373ea00f539aa9434b7191356ec8f346b
Han Zhang <hanzhang@Hans-MacBook-Pro.local>
2023-04-13 09:21:47 -0700

    add evaluation mode, which read the output json and evalute its plan.

---
+++ inc/Evaluation.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "MAPFPlanner.h"
+
+class DummyPlanner: public MAPFPlanner
+{
+private:
+  std::vector<std::deque<Action>> agent_plans;
+public:
+
+
+  DummyPlanner(std::string fname): MAPFPlanner() {
+    load_plans(fname);
+  };
+	~DummyPlanner(){}
+
+  void load_plans(std::string fname);
+
+  virtual std::vector<Action> plan(int time_limit);
+
+};
