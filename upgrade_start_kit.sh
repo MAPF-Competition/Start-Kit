f647c42f4f4006e79e517a31b100bd37ae4b7cef
nobodyczcz <jianshen858@gmail.com>
2023-06-29 11:45:24 +1000

    bug fix in upgrade script

--- upgrade_start_kit.sh
+++ upgrade_start_kit.sh
@@ -7,11 +7,11 @@ git fetch upstream --no-tags main
 git restore --staged --worktree --source=upstream/main utils/upgrade_file_list.txt
 
 files=""
-while read -r line;
+while IFS='' read -r line;
 do
-   files="$files $line" ;
+   files="$files \"$line\"" ;
 done < utils/upgrade_file_list.txt
-
+echo restore files: $files
 git restore --staged --worktree --source=upstream/main $files
 
 git commit -m "updated to latest startkit"
