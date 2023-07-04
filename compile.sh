7f4fcbace6bbea2212f4fe2feec1260e1b38e018
Teng Guo <2274880117@qq.com>
2023-07-04 11:33:18 -0400

    modified python

--- compile.sh
+++ compile.sh
@@ -4,14 +4,14 @@ mkdir build
 
 # build exec for cpp
 
-# cd build
-# cmake ../
-# make -j
+cd build
+cmake ../
+make -j
 
 
 # build exec for python
 
-cd build
-cmake ../ -DPYTHON=true
-make -j
-cp *so ../*.so  #make sue the library is in the working directory
+# cd build
+# cmake ../ -DPYTHON=true
+# make -j
+# cp *so ../*.so  #make sue the library is in the working directory
