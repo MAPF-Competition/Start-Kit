e49261331e304379e3de190954c65cb44ca4993d
Anthony Oon <anthony.zh.oon@gmail.com>
2024-02-15 16:19:38 +1100

    Implemented SimpleCoordinator as a pass-through that simply replans on any failure

--- inc/Coordinator.hpp
+++ inc/Coordinator.hpp
@@ -3,27 +3,30 @@
 #include "ExecutionSimulator.h"
 #include "ScheduleTable.hpp"
 #include <deque>
+#include <vector>
 
 class BaseCoordinator {
 public:
   BaseCoordinator(int batch_length, int num_agents, ActionExecutor &executor,
                   ActionModelWithRotate &model)
       : batch_length_(batch_length), num_agents_(num_agents),
-        executor_(executor), agent_progress_(vector<int>(num_agents)),
+        executor_(executor),
         model_(model) {}
 
-  void send_action(vector<Action> &next_action);
+  // Send an action for all agents to the Coordinator
+  virtual void send_action(vector<Action> &next_action);
 
   // Simulates all queued actions
-  vector<State> do_actions(vector<State> &curr_states,
-                           vector<std::deque<Action>> &next_actions);
+  virtual vector<State> do_action(const vector<State> &curr_states);
+
+  // Returns true if after updating the Coordinator wants to replan instead of repair 
+  virtual bool update_needs_replan(const vector<bool> &successed);
 
   inline int get_batch_size() { return batch_length_; }
 
 protected:
   int batch_length_;
   int num_agents_;
-  vector<int> agent_progress_;
   ActionExecutor &executor_;
   ActionModelWithRotate &model_;
 };
@@ -32,9 +35,42 @@ class SimpleCoordinator : BaseCoordinator {
 public:
   SimpleCoordinator(int batch_length, int num_agents, ActionExecutor &executor,
                     ActionModelWithRotate &model)
-      : BaseCoordinator(batch_length, num_agents, executor, model){};
+      : action_queues_(vector<deque<Action>>(num_agents)), BaseCoordinator(batch_length, num_agents, executor, model){};
+
+  void send_action(vector<Action> &next_action) override {
+    for (int i = 0; i < num_agents_; i++) {
+      action_queues_.at(i).push_back(next_action.at(i));
+    }
+    return;
+  }
+
   
+  vector<State> do_action(const vector<State> &curr_states) override {
+    vector<State> next_states(num_agents_);
+    for (int i = 0; i < num_agents_; i++) {
+      next_states.at(i) = model_.result_state(curr_states.at(i), action_queues_.at(i).front());
+    }
+    return next_states;
+  }
+
+// Returns true if the plan fails any action and forces a replan 
+  bool update_needs_replan(const vector<bool> &successed) override {
+    for (int i = 0; i < num_agents_; i++) {
+      if (successed.at(i)) {
+        action_queues_.at(i).pop_front();
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+private:
+vector<deque<Action>> action_queues_;
 };
+
+
 class MCPCoordinator : BaseCoordinator {
 public:
   MCPCoordinator(int batch_length, int num_agents, ActionExecutor &executor,
