ea13cf6ce0277f7e39da54d07f455bdc2d90fec1
Teng Guo <2274880117@qq.com>
2024-08-06 12:32:22 -0400

    update pyentry

--- python/pyEntry.hpp
+++ python/pyEntry.hpp
@@ -23,43 +23,4 @@ public:
         planner = new pyMAPFPlanner(env);
         scheduler = new pyTaskScheduler(env);
     }
-
-    void initialize(int preprocess_time_limit)
-    {
-        planner->initialize(preprocess_time_limit);
-    }
-
-    void compute(int time_limit, std::vector<Action> &plan, std::vector<int> &proposed_schedule)
-    {
-
-        // first call task schedule
-        scheduler->plan(time_limit, proposed_schedule);
-
-        // then update the task location to planner
-        for (size_t i = 0; i < proposed_schedule.size(); i++)
-        {
-            env->goal_locations[i].clear();
-            int t_id = proposed_schedule[i];
-            if (t_id == -1)
-                continue;
-            Task *task_ptr = nullptr;
-            for (Task &task : env->task_pool)
-            {
-                if (task.task_id == t_id)
-                {
-                    task_ptr = &task;
-                    break;
-                }
-            }
-            if (task_ptr == nullptr)
-            {
-                continue;
-            }
-            int i_loc = task_ptr->idx_next_loc;
-            env->goal_locations[i].push_back({task_ptr->locations.at(i_loc), task_ptr->t_revealed});
-        }
-
-        // then call planner
-        planner->plan(time_limit, plan);
-    }
 };
