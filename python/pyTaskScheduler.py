359c86fd4ddd2bcdec9aa5eeea51f66dd8181b33
Teng Guo <2274880117@qq.com>
2024-08-06 12:19:40 -0400

    update python

---
+++ python/pyTaskScheduler.py
@@ -0,0 +1,33 @@
+import MAPF
+import numpy
+from typing import Dict, List, Tuple,Set
+
+
+class pyTaskScheduler:
+    def __init__(self, env):
+        self.env=env
+  
+  
+    def initialize(self, preprocess_time_limit:int):
+        """
+        Initialize the task scheduler
+        """
+        print("python scheduler!!")
+    
+    def plan(self,time_limit:int):
+        """
+        Plan the task schedule
+        """
+        proposed_schedule=[None for i in range(self.env.num_of_agents)]
+        i_task=0
+        for i in range(self.env.num_of_agents):
+            if self.env.curr_task_schedule[i]==-1:
+                while i_task<len(self.env.task_pool):
+                    if self.env.task_pool[i_task].agent_assigned==-1:
+                        proposed_schedule[i]=self.env.task_pool[i_task].task_id
+                        self.env.task_pool[i_task].agent_assigned=i
+                        break
+            else:
+                proposed_schedule[i]=self.env.curr_task_schedule[i]
+        return proposed_schedule
+    
