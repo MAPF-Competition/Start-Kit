c0ca07c0d8fbd31b66f4ce30d2597ea291d0c4c9
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2026-02-18 10:52:23 +1100

    mcp implementation

--- inc/Simulator.h
+++ inc/Simulator.h
@@ -53,7 +53,9 @@ public:
         }
     };
 
-    void process_new_plan(int sync_time_limit,int overtime_runtime, vector<Action>& plan) ;
+    bool initialise_executor(int preprocess_time_limit);
+
+    void process_new_plan(int sync_time_limit,int overtime_runtime, vector<Action>& plan);
 
     vector<State> move(int move_time_limit, vector<Action>& next_actions);
 
