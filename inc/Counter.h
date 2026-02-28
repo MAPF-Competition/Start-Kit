566edf8b16b9ccae544fbf2b791657a049c5fbe7
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2026-02-28 12:34:06 +1100

    add counter as input

--- inc/Counter.h
+++ inc/Counter.h
@@ -5,7 +5,7 @@ struct Counter
 {
     int count;
     int maxCount;
-    Counter(): count(0), maxCount(10) {}
+    Counter(int max_counter=10): count(0), maxCount(max_counter) {}
     bool tick()
     {
         // Returns true if reaches maxCount and resets. The agent moves to the next location when this function returns true. Otherwise the agent is on the edge.
