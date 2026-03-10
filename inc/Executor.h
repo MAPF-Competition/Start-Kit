d1f3f61ccc66fef3b1224384c16ee075d66c9919
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2026-03-11 09:16:57 +1100

    temp implementation for window process new plan

--- inc/Executor.h
+++ inc/Executor.h
@@ -18,6 +18,7 @@ public:
     virtual void initialize(int preprocess_time_limit);
     // return the predicted states after processing the new plan, and staged actions for each agent
     virtual vector<State> process_new_plan(int sync_time_limit, Plan & plan, vector<vector<Action>> & staged_actions);
+    vector<State> process_new_plan_window(int sync_time_limit, Plan& plan_struct, vector<vector<Action>> & staged_actions);
     // return the next execution command for each agent based on the current state and staged actions
     virtual void next_command(int exec_time_limit, std::vector<ExecutionCommand> & agent_command);
 
@@ -29,6 +30,6 @@ public:
     ProcessPlanType process_plan_type = APPEND_WINDOW; //the default process plan type is to append windoed actions to staged actions, window = min_planner_communication_time/simulator_time_limit, which means we will only execute the part of the plan that can be executed within the communication time limit.
     int window_size;
 
-    bool mcp(int agent_id, vector<bool> & curr_decision, std::vector<ExecutionCommand> & agent_command);
+    bool mcp(int agent_id, std::vector<ExecutionCommand> & agent_command);
 
 };
