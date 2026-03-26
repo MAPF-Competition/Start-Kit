84d711fc8168411ef6c4b983d67b01af876ba42c
DuoZhangRobotics <976539567@qq.com>
2026-03-25 21:24:27 -0400

    hashmap working

--- default_planner/scheduler.cpp
+++ default_planner/scheduler.cpp
@@ -9,7 +9,7 @@ std::unordered_set<int> free_tasks;
 void schedule_initialize(int preprocess_time_limit, SharedEnvironment* env)
 {
     // cout<<"schedule initialise limit" << preprocess_time_limit<<endl;
-    DefaultPlanner::init_heuristics(env);
+    // DefaultPlanner::init_heuristics(env);
     mt.seed(0);
     return;
 }
@@ -60,7 +60,7 @@ void schedule_plan(int time_limit, std::vector<int> & proposed_schedule,  Shared
             // iterate over the locations (errands) of the task to compute the makespan to finish the task
             // makespan: the time for the agent to complete all the errands of the task t_id in order
             for (int loc : env->task_pool[t_id].locations){
-                dist += DefaultPlanner::get_h(env, c_loc, loc);
+                dist += DefaultPlanner::get_h(env, c_loc, loc, true);
                 c_loc = loc;
             }
 
